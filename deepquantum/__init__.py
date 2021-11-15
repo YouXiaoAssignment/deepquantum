6da42be60b92114de6e65347fd0da3a93bffe9b6
shishunyoung <shishunyang@foxmail.com>
2021-11-15 15:02:24 +0800

    20211115circuit update

--- deepquantum/__init__.py
+++ deepquantum/__init__.py
@@ -1,3 +1,9 @@
-from .gates import qgate, qmath
+from .gates import qgate, qmath, qcircuit
 from .layers import qlayer
 from .embeddings import qembedding
+
+from .gates.qcircuit import Circuit
+from .gates.qoperator import *
+from .gates.qmath import *
+
+from .layers.qlayers import * 
